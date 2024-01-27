2f53c3f17b216e010dd42c0913f678312cb35ff2
mr-ema <ema.a.v.c2017@gmail.com>
2024-01-26 21:22:59 -0300

    update to zig 0.11.0 - 0.12.0

--- build.zig
+++ build.zig
@@ -1,26 +1,23 @@
 const std = @import("std");
 const raylib = @import("libs/raylib/src/build.zig");
 
-pub fn build(b: *std.Build) void {
-    const cnake_flags = &[_][]const u8{
-        "-std=gnu99",
-        "-D_GNU_SOURCE",
-    };
-
+// Working in zig 0.12.0
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "cnake",
-        .root_source_file = .{ .path = "src/main.c" },
         .target = target,
         .optimize = optimize,
     });
 
-    // Working in zig 0.11.0
+    exe.linkLibrary(try raylib.addRaylib(b, target, optimize, .{}));
     exe.addIncludePath(.{ .path = "libs/raylib/src" });
-    exe.linkLibrary(raylib.addRaylib(b, target, optimize, .{}));
-    exe.addCSourceFiles(&.{"src/controls.c"}, cnake_flags);
+    exe.addCSourceFiles(.{
+        .files = &.{ "src/main.c", "src/controls.c" },
+        .flags = &.{ "-std=gnu99", "-D_GNU_SOURCE" },
+    });
 
     b.installArtifact(exe);
 
@@ -34,14 +31,14 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    // const unit_tests = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+    // const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_unit_tests.step);
 }
